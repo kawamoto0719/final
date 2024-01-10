cb18cda983b776ad824aad4976b6ec32a649267c
kawamoto0719 <148738656+kawamoto0719@users.noreply.github.com>
2024-01-10 11:27:57 +0900

    Create touroku-input.php
    touroku-input

---
+++ src/touroku-input.php
@@ -0,0 +1,19 @@
+<?php require "db-connect.php" ?>
+<?php require "head.php" ?>
+
+<form action="touroku-output.php"  method="post">
+
+
+<h2>新規チーム登録<h2>
+    <p>チーム名</p>
+    <input type="text" name="team_name">
+    <p>パスワード</p>
+    <input type="password" name="password">
+    <p>チーム本拠地</p>
+    <input type="text" name="team_home">
+    <p>チームオーナー</p>
+    <input type="text" name="team_owner">
+
+    <p><input type="submit" value="登録">
+</body>
+</html>
