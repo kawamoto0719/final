91c1398cf368092b2e4095300bb73b1f4a94296a
kawamoto0719 <148738656+kawamoto0719@users.noreply.github.com>
2024-01-10 11:26:32 +0900

    Create db-connect.php
    src

---
+++ src/db-connect.php
@@ -0,0 +1,8 @@
+<?php
+    const SERVER = 'mysql220.phy.lolipop.lan';
+    const DBNAME = 'LAA1516813-final';
+    const USER = 'LAA1516813';
+    const PASS = 'Pass0719';
+
+    $connect = 'mysql:host='. SERVER . ';dbname='. DBNAME . ';charset=utf8';
+?>
